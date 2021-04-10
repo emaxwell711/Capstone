334785c556bdbba24345e81c72698e7c4a08e566
Emily Maxwell <emily.maxwell@slu.edu>
2021-04-10 08:13:40 -0500

    test

---
+++ app/src/main/java/com/example/falluserstudy2019_v2/Activities/CSV_Reader.java
@@ -0,0 +1,46 @@
+package com.example.falluserstudy2019_v2.Activities;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class CSV_Reader {
+
+    public static void main(String[] args) {
+        String fileName= "read_ex.csv";
+        File file= new File(fileName);
+
+        List<List<String>> lines = new ArrayList<>();
+        Scanner inputStream;
+
+        try{
+            inputStream = new Scanner(file);
+
+            while(inputStream.hasNext()){
+                String line= inputStream.next();
+                String[] values = line.split(",");
+                // this adds the currently parsed line to the 2-dimensional string array
+                lines.add(Arrays.asList(values));
+            }
+
+            inputStream.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // the following code lets you iterate through the 2-dimensional array
+        int lineNo = 1;
+        for(List<String> line: lines) {
+            int columnNo = 1;
+            for (String value: line) {
+                System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
+                columnNo++;
+            }
+            lineNo++;
+        }
+    }
+
+}
